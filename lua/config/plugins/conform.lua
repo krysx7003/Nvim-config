192c1191118184d456f21595f86c9084c478b63a
krysx7003 <krysx7003@gmail.com>
2025-06-28 18:08:42 +0200

    Added conform moved all formaters there

---
+++ lua/config/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"stevearc/conform.nvim",
+
+		opts = {
+			notify_on_error = true,
+			format_on_save = function(bufnr)
+				local disable_filetypes = {}
+				return {
+					timeout_ms = 500,
+					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				}
+			end,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang_format" },
+				python = { "isort", "black" },
+			},
+		},
+		formatters = {
+			clang_format = {
+				prepend_args = { "--style=file", "--fallback-style=LLVM" },
+			},
+		},
+	},
+}
